49daeb3c59d4d47d3bd4c766422c3aad47a74fb4
Aymerick <aymerick@jehanne.org>
2015-04-15 14:58:05 +0200

    Adds test: TestImportantPriority

--- inliner/inliner_test.go
+++ inliner/inliner_test.go
@@ -87,6 +87,49 @@ func TestInlineStylePriority(t *testing.T) {
 	}
 }
 
+// !important has highest priority
+func TestImportantPriority(t *testing.T) {
+	input := `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<style type="text/css">
+  p#test {
+    color: #000;
+  }
+  p {
+    color: #eee !important;
+  }
+</style>
+  </head>
+  <body>
+    <p id="test" style="color: #222;">
+      Inline me please!
+    </p>
+</body>
+</html>`
+
+	expectedOutput := `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+  </head>
+  <body>
+    <p id="test" style="color: #eee;">
+      Inline me please!
+    </p>
+
+</body></html>`
+
+	output, err := Inline(input)
+	if err != nil {
+		t.Fatal("Failed to inline html:", err)
+	}
+
+	if output != expectedOutput {
+		t.Fatal(fmt.Sprintf("CSS inliner error\nExpected:\n\"%s\"\nGot:\n\"%s\"", expectedOutput, output))
+	}
+}
+
 // Pseudo-class selectors can't be inlined
 func TestNotInlinable(t *testing.T) {
 	input := `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
